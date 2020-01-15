c4397439d1b838c6dcd3128d9c509292d3c4e001
nmonga1 <nmonga1@umd.edu>
2020-01-14 19:28:59 -0500

    img on ssr for model

--- theme/src/components/modelviewer/index.tsx
+++ theme/src/components/modelviewer/index.tsx
@@ -5,8 +5,9 @@ interface ModelViewerProps {
   url: string;
 }
 
+const isBrowser = typeof window !== 'undefined';
+
 const ModelViewer: FunctionComponent<ModelViewerProps> = ({url=''}) => {
-  /*
   const [scrollProgress, setScrollProgress] = useState<number>(0);
   const scrollListener = () => {
     // const windowScrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
@@ -21,22 +22,27 @@ const ModelViewer: FunctionComponent<ModelViewerProps> = ({url=''}) => {
   }, []);
   // const orbit = `calc(30deg - env(window-scroll-y) * 100deg) 75deg 2.5m`
   // camera-orbit={(30 - scrollProgress) +'deg 75deg 2.5m'}
-  */
-  return (
-    <model-viewer src={url}
-      camera-controls
-      camera-orbit={'30deg 75deg 2.5m'}
-      preload 
-      id="mview"
-      reveal-when-loaded
-      interaction-poliicy="allow-when-focused"
-      interaction-prompt="basic"
-      style={{height: '343px', width: '555px'}}
-      alt="A low-poly 3D model of a pizza box"
-      quick-look-browsers="safari chrome"
-      background-color="#b52e31">
-    </model-viewer>
-  );
+  if (isBrowser) {
+    return (
+      <model-viewer src={url}
+        camera-controls
+        camera-orbit={(30 - scrollProgress) +'deg 75deg 2.5m'}
+        preload 
+        id="mview"
+        reveal-when-loaded
+        interaction-policy="allow-when-focused"
+        interaction-prompt="basic"
+        style={{height: '343px', width: '555px'}}
+        alt="A low-poly 3D model of a pizza box"
+        quick-look-browsers="safari chrome"
+        background-color="#b52e31">
+      </model-viewer>
+    );
+  } else {
+    return (
+      <img src="https://jetlaglabs.github.io/assets/img/vr.svg" alt="vr" width="555px" height="343px" />
+    )
+  }
 };
 
 export default ModelViewer;
